921aa3caf931594c3ef236febf73b5a067c91027
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-05-16 00:19:24 +0300

    layout page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.head')
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Navbar -->
+    @include('layouts.navbar')
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    @include('layouts.sidebar')
+
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">@yield('header', 'users')</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.content-header -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2020-2024 <a href="https://graspstudios.com/">Grasp Studios</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Phone:</b> <a href="tel:+201201222298">+201201222298</a>
+        </div>
+    </footer>
+</div>
+@include('layouts.scripts')
+</body>
+</html>

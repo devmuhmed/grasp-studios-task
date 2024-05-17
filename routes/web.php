4aaf15136007f1d06dfe70cb456aa5e664435e5a
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-05-17 15:49:49 +0300

    project done

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout');
-});
+    return redirect()->route('task.index');
+})->middleware('auth');
 
 Auth::routes();
 
